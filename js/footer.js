ad8860de6d28b323e1607db050f87524ba549578
AndresAB <andres.are.96@gmail.com>
2024-03-31 22:22:54 -0600

    se sintetiza el footer

---
+++ js/footer.js
@@ -0,0 +1,30 @@
+const footer = document.getElementById('footer');
+
+footer.innerHTML=`
+<div class="row">
+<!-- Logo-->
+<div class="container-img-footer col-12 col-sm-12 col-md-4 col-lg-4 d-flex flex-row justify-content-around aling-items-center">
+  <img src="src/img/img-logo/LogoEsfera.png " alt="" class="red-social">
+</div>
+<!-- Logo pagina-->
+
+<!-- Redes sociales-->
+<div class="container-red-social ol-12 col-sm-12 col-md-4 col-lg-4 d-flex flex-row justify-content-center justify-content-md-around aling-items-center ">
+  <a href="#">
+    <img src="src/img/img-logo/InstagramBordado.png" alt="" class="red-social">
+  </a>
+  <a href="#">
+    <img src="src/img/img-logo/Logofacebook.png " alt="" class="red-social">
+  </a>
+</div>
+<!-- Redes sociales-->
+
+<!-- Datos de contacto -->
+  <div class="ol-12 col-sm-12 col-md-4 col-lg-4 d-flex flex-column justify-content-around aling-items-center">
+    <p>Telefono: 55-5555-5555</p>
+    <p>Email: bordados_esime@gmail.com</p>
+  </div>
+<!-- Datos de contacto -->
+</div>
+</div>
+</div>`;
