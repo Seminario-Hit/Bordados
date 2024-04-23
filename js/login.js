603126af824c73bef0ef0a50194e7d8a8b986eb6
AndresAB <andres.are.96@gmail.com>
2024-04-23 01:05:56 -0600

    se agrega peticion fetch para iniciar con el login y la accesstoken

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
 const inputs=document.querySelectorAll('input');
-
-
+const formulario = document.getElementById('login');
+const email = document.getElementById('email');
+const pass = document.getElementById('password');
 
 const expresiones ={
     email: /^[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,4}$/,
@@ -43,3 +44,44 @@ inputs.forEach((input) => {
 
 })
 
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    var url ='http://localhost:8080/login/';
+
+        fetch( url,{
+            method: 'POST',
+            body:
+            JSON.stringify({
+                userEmail: email.value,
+                password: pass.value
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+            
+        }).then(response => response.json())
+
+            .then(respuesta => {
+                if(respuesta) 
+                {
+                    console.log(respuesta);
+                    Swal.fire({
+                        icon:'success',
+                        title: 'Exito',
+                        text: 'Registro exitoso',
+                    })
+                ;}
+                else{
+                    Swal.fire({
+                        icon:'error',
+                        title: 'Registro Fallido',
+                        text: 'La direccion de email ya esta registrada',
+                    })
+                }
+            })
+        .catch(error => {
+            console.log('Datos incorrectos', error);
+        });
+    }
+);
