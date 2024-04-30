603e9e006834f76e505d195eeb76f80db583b52e
AndresAB <andres.are.96@gmail.com>
2024-04-29 18:32:51 -0600

    se agrega token al session storage

--- js/login.js
+++ js/login.js
@@ -49,6 +49,7 @@ formulario.addEventListener('submit', (e) => {
 
     var url ='http://localhost:8080/login/';
 
+    if(campos.email && campos.password){
         fetch( url,{
             method: 'POST',
             body:
@@ -63,25 +64,49 @@ formulario.addEventListener('submit', (e) => {
         }).then(response => response.json())
 
             .then(respuesta => {
-                if(respuesta) 
+                if(respuesta.accessToken.length!==0) 
                 {
                     console.log(respuesta);
                     Swal.fire({
                         icon:'success',
-                        title: 'Exito',
-                        text: 'Registro exitoso',
-                    })
-                ;}
+                        title: 'Éxito',
+                        text: 'Inicio de sesión exitoso'
+                    });
+                window.sessionStorage.setItem("token", JSON.stringify(respuesta));
+                window.sessionStorage.setItem("userLogged", JSON.stringify({ userEmail: email.value }));
+                    formulario.reset();
+                    setTimeout(()=>{window.location.href = "../index.html"},2000);
+                }
                 else{
                     Swal.fire({
                         icon:'error',
                         title: 'Registro Fallido',
-                        text: 'La direccion de email ya esta registrada',
+                        text: 'Usuario y/o contraseña no validos',
                     })
+                    formulario.reset();
                 }
             })
         .catch(error => {
-            console.log('Datos incorrectos', error);
+            console.error('Error', error);
         });
+
+        document.querySelectorAll('.is-valid').forEach((icono) => {
+            icono.classList.remove('is-valid');
+        });
+    
+            Object.keys(campos).forEach(campo => {
+                campos[campo] = false;
+            })
+       
+            
+        } else {
+            document.getElementById('alert-danger').classList.add('form--mensaje-activo');
+            setTimeout(() => {
+                document.getElementById('alert-danger').classList.remove('form--mensaje-activo');
+            }, 5000);
+        
+    }
+
+        
     }
 );
